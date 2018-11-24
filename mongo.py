a22c1be3fc664f408324222967fc0c5030313f76
Tommi Saarinen <tommi.saarinen@aalto.fi>
2018-11-24 12:35:02 +0200

    Add functions to read collections from DB

--- mongo.py
+++ mongo.py
@@ -8,15 +8,15 @@ DB_PORT = 15434
 DB_USER = "tommi"
 DB_PASS = os.environ.get("PASSWD")
 
-print(DB_NAME)
-print(DB_HOST)
-print(DB_PORT)
-print(DB_USER)
-print(DB_PASS)
-
 client = MongoClient(DB_HOST, DB_PORT)
 db = client[DB_NAME]
 db.authenticate(DB_USER, DB_PASS)
 
-testcollection = db.collection
-print(testcollection.find_one())
+def getAllBookings():
+  return db.bookings.find({})
+
+def getAllStations():
+  return db.stations.find({})
+
+def getAllBookings():
+  return db.transports.find({})  
